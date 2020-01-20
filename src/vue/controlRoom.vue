d5ad05c116b93eb7a2569763b0d928e46cf2c03b
CAOJIAYU <xiaokeaicg@hotmail.com>
2020-01-20 10:58:34 +0800

    控制台安排

---
+++ src/vue/controlRoom.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="qrcode">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      datalist: [] //数据信息
+    };
+  },
+
+  computed: {
+
+  },
+  methods: {
+
+  },
+  components: {
+    
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.qrcode {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-bottom: 1rem;
+}
+</style>
